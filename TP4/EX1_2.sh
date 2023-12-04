8a036cdab75e934f6528c3d0b2dfb0e5ca00d77d
Thibault GOUNANT <e202068p@ird116-05-l.etu.polytech.univ-nantes.prive>
2023-12-04 12:08:03 +0100

    Add shell ex

---
+++ TP4/EX1_2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "1. A B"
+echo "2. L’ensemble {1, 2, 3} est inclus dans N"
+echo "3. Elle s’écria : \"Ciel mon mari !\""
+echo "4. \$HOME = chemin où chemin est le chemin de votre répertoire de login d’après le bash"
+echo "5. 19 * 216 = produit où produit est la valeur du produit calculée par le bash"
